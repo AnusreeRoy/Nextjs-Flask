2cf0eb78edbf6121f1f79f1139b6ac7a9d49bd75
AnusreeRoy <119887004+AnusreeRoy@users.noreply.github.com>
2023-08-02 00:35:56 +0600

    Update index.js

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,24 +1,36 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 
 function Index() {
+  const [text, setText] = useState(null);
 
-  const[text, setText]=useState(null)
-  useEffect(()=>{
-  fetch("http://localhost:8080/api/home")
-  .then((response) => response.json())
-  .then((data) => {
-    setText(data.message)
-    console.log(data);
-  });
-  },[])
+  useEffect(() => {
+    const corsProxyUrl = 'https://cors-anywhere.herokuapp.com/';
+    const apiUrl = 'https://test-v5o1.onrender.com/';
 
+    fetch(corsProxyUrl + apiUrl)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then((data) => {
+        // setText(data.message);
+        console.log(data);
+      })
+      .catch((error) => {
+        console.error('Error fetching data:', error);
+      });
+  }, []);
 
   return (
-    <div>{text}</div>
-    // <div>
-    //   <p>Hello</p>
-    // </div>
-  )
+    <>
+      {/* <div>{text}</div> */}
+      <div>
+        <p>Hello</p>
+      </div>
+    </>
+  );
 }
 
-export default Index
+export default Index;
